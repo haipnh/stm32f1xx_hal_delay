afb2694e5dc20a377fca6ed604faae43832a0135
hatodev <haiphamnguyenhoang@gmail.com>
2018-11-26 22:53:34 +0700

    first init

---
+++ stm32f1xx_hal_delay.c
@@ -0,0 +1,28 @@
+#include "stm32f1xx_hal_delay.h"
+
+void DelayUs(uint32_t us){
+	uint16_t  t0 = TIM2->CNT;
+	for (;;) {
+		int  t = TIM2->CNT;
+		if (t < t0)
+			t += 0x10000;
+
+		if (us < t - t0)
+			return;
+
+		us -= t - t0;
+		t0  = t;
+	}
+}
+
+void DelayMs(uint32_t ms){
+	DelayUs(ms * 1000);
+}
+
+void DelayInit(void){
+	  __HAL_RCC_TIM2_CLK_ENABLE();
+    TIM2->PSC = (HAL_RCC_GetPCLK2Freq() / 1000000) - 1;
+    TIM2->ARR = 0xFFFF;
+    TIM2->CR1 = TIM_CR1_CEN;
+}
+
---
+++ stm32f1xx_hal_delay.h
@@ -0,0 +1,11 @@
+#ifndef __STM32F1XX_HAL_DELAY_H
+#define __STM32F1XX_HAL_DELAY_H
+
+#include "stm32f1xx_hal.h"
+
+void DelayUs(uint32_t us);
+void DelayMs(uint32_t ms);
+void DelayInit(void);
+
+#endif
+
